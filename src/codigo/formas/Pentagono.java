f040604c90544c96ee2cf87cc5f08fe1c225358b
Jorge Cisneros <fraxito@gmail.com>
2020-01-23 18:40:30 +0100

    Ahora dibuja Pentágonos correctamente

---
+++ src/codigo/formas/Pentagono.java
@@ -0,0 +1,16 @@
+
+package codigo.formas;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jorge Cisneros
+ */
+public class Pentagono extends Forma{
+    
+    public Pentagono(int _posX, int _posY, int _lados, Color _color, boolean _relleno) {
+        super(_posX, _posY, 5, _color, _relleno);
+    }
+    
+}
